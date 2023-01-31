6a1431ddc117a6ddd896608310df41a108fa8a50
Gustavo Henrique Araújo de Paula <gustavo_henriquedepaula@hotmail.com>
2023-01-30 23:13:49 -0300

    Aula 1

---
+++ lib/app/pages/splash/splash_page.dart
@@ -0,0 +1,48 @@
+import 'package:dw9_delivery_app/app/core/ui/helpers/size_extensions.dart';
+import 'package:dw9_delivery_app/app/core/ui/widgets/delivery_button.dart';
+import 'package:flutter/material.dart';
+
+import '../../core/config/env/env.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Splash'),
+      ),
+      body: Column(
+        children: [
+          Container(),
+          DeliveryButton(
+            width: 200,
+            height: 200,
+            label: Env.i['backend_base_url'] ?? '',
+            onPressed: () {},
+          ),
+          Text(MediaQuery.of(context).size.width.toString()),
+          Text(context.screenWidth.toString()),
+          Row(
+            children: [
+              Container(
+                color: Colors.red,
+                width: context.percentWidth(.6),
+                height: 200,
+              ),
+              Container(
+                color: Colors.blue,
+                width: context.percentWidth(.4),
+                height: 200,
+              ),
+            ],
+          ),
+          TextFormField(
+            decoration: const InputDecoration(labelText: 'text'),
+          ),
+        ],
+      ),
+    );
+  }
+}
